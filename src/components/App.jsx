bc256549e50afeef05de2f9e8f7df9ee2f1d6e64
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-05-04 15:50:24 +0300

    apisearch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { apiSearch } from '../api/apiSearch';
+
+export class App extends Component {
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12'
+    )
+      .then(res => res.json())
+      .then(console.log);
+  }
+
+  render() {
+    return <div style={{}}> Hello my!!!</div>;
+  }
+}
